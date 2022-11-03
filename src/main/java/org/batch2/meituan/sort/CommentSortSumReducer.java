9b888ce18c7e712c80bd1aef2a8cff9997f3dd46
GakshLimei <1260671954@qq.com>
2022-11-03 15:51:29 +0800

    添加排序功能

--- src/main/java/org/batch2/meituan/sort/CommentSortSumReducer.java
+++ src/main/java/org/batch2/meituan/sort/CommentSortSumReducer.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 /**
  * @author:Gary
- * @date: 2022年11月25日 14:48
+ * @date: 2022年11月2日 14:48
  * @desc:
  */
 public class CommentSortSumReducer extends Reducer<CommentCountBean2, Text, Text, CommentCountBean2> {
@@ -20,13 +20,10 @@ public class CommentSortSumReducer extends Reducer<CommentCountBean2, Text, Text
         /**
          * 排序好之后 reduce会进行分组操作，分组规则是判断key是否相等
          * 本业务中，使用自定义对象作为key，并且没有重写分组规则，默认就会比较对象的地址，导致每个kv队就是一组
-         * <CommentCountBean3,加州> --------------><CommentCountBean3,Iterable[加州]>
-         * <CommentCountBean3,德州> --------------><CommentCountBean3,Iterable[德州]>
+         * <CommentCountBean2,加州> --------------><CommentCountBean2,Iterable[加州]>
+         * <CommentCountBean2,德州> --------------><CommentCountBean2,Iterable[德州]>
          */
-//        String state_name = null;
-//        for (Text state:values){
-//            state_name = state.toString()
-//        }
+
         Text outValues = values.iterator().next(); //每执行一次reduce就会执行迭代器的next(); 拿到 州 名
         CommentCountBean2 value = key;
         context.write(outValues, key);//key:Alabama value(319025 1165)
