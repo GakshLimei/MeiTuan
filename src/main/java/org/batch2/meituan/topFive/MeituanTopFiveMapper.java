a36e09a99152ce1ddfee0e9970a1835828b6176d
邓立文 <664514687@qq.com>
2022-11-22 20:55:38 +0800

    变更

--- src/main/java/org/batch2/meituan/topFive/MeituanTopFiveMapper.java
+++ src/main/java/org/batch2/meituan/topFive/MeituanTopFiveMapper.java
@@ -18,19 +18,19 @@ public class MeituanTopFiveMapper extends Mapper<LongWritable, Text, MeituanBean
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] fields = value.toString().split(",");
-        //
-        //
+        // 封装数据：市  外卖店  月销量
+        //通过索引值获取市的名字
         String city = fields[1];
-        //
+        //通过索引值获得外卖店的名字
         String name = fields[2];
-        //
+        //通过索引值获得月销售量
         String month_sales = fields[3];
 
         outKey.set(city, name, Long.parseLong(month_sales));
 
         outValue.set(Long.parseLong(month_sales));
 
-        //
+        //输出
         context.write(outKey, outValue);
     }
 }
