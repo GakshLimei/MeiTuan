b24a666504d4029b77da54361711b6c72adf24d4
GakshLimei <1260671954@qq.com>
2022-12-04 03:25:43 +0800

    可视化

--- src/main/java/org/batch2/viewdata/meituanviewdata/js/index.js
+++ src/main/java/org/batch2/viewdata/meituanviewdata/js/index.js
@@ -1,15 +1,241 @@
 $(function(){
 
+	echarts_1();
+	echarts_3();
 	map();
-
     //leidatu();
 	wuran2()
     wuran();
     huaxing();
-	zhexian();
+	// zhexian();
 
+	function echarts_1() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echarts_1'));
 
+        var data = [
+            {value: 12,name: '行业一'},
+            {value: 13,name: '行业二'},
+            {value: 70,name: '行业三'},
+            {value: 52,name: '行业四'},
+            {value: 35,name: '行业五'}
+        ];
 
+        option = {
+            backgroundColor: 'rgba(0,0,0,0)',
+            tooltip: {
+                trigger: 'item',
+                formatter: "{b}: <br/>{c} ({d}%)"
+            },
+            color: ['#af89d6', '#4ac7f5', '#0089ff', '#f36f8a', '#f5c847'],
+            legend: { //图例组件，颜色和名字
+                x: '70%',
+                y: 'center',
+                orient: 'vertical',
+                itemGap: 12, //图例每项之间的间隔
+                itemWidth: 10,
+                itemHeight: 10,
+                icon: 'rect',
+                data: ['行业一', '行业二', '行业三', '行业四', '行业五'],
+                textStyle: {
+                    color: [],
+                    fontStyle: 'normal',
+                    fontFamily: '微软雅黑',
+                    fontSize: 12,
+                }
+            },
+            series: [{
+                name: '行业占比',
+                type: 'pie',
+                clockwise: false, //饼图的扇区是否是顺时针排布
+                minAngle: 20, //最小的扇区角度（0 ~ 360）
+                center: ['35%', '50%'], //饼图的中心（圆心）坐标
+                radius: [50, 75], //饼图的半径
+                avoidLabelOverlap: true, ////是否启用防止标签重叠
+                itemStyle: { //图形样式
+                    normal: {
+                        borderColor: '#1e2239',
+                        borderWidth: 2,
+                    },
+                },
+                label: { //标签的位置
+                    normal: {
+                        show: true,
+                        position: 'inside', //标签的位置
+                        formatter: "{d}%",
+                        textStyle: {
+                            color: '#fff',
+                        }
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                data: data
+            }, {
+                name: '',
+                type: 'pie',
+                clockwise: false,
+                silent: true,
+                minAngle: 20, //最小的扇区角度（0 ~ 360）
+                center: ['35%', '50%'], //饼图的中心（圆心）坐标
+                radius: [0, 40], //饼图的半径
+                itemStyle: { //图形样式
+                    normal: {
+                        borderColor: '#1e2239',
+                        borderWidth: 1.5,
+                        opacity: 0.21,
+                    }
+                },
+                label: { //标签的位置
+                    normal: {
+                        show: false,
+                    }
+                },
+                data: data
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+
+	function echarts_3() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echarts_3'));
+
+        option = {
+
+            tooltip : {
+                trigger: 'axis'
+            },
+            legend: {
+                orient: 'vertical',
+                data:['简易程序案件数']
+            },
+            grid: {
+                left: '3%',
+                right: '3%',
+                top:'8%',
+                bottom: '5%',
+                containLabel: true
+            },
+            color:['#a4d8cc','#25f3e6'],
+            toolbox: {
+                show : false,
+                feature : {
+                    mark : {show: true},
+                    dataView : {show: true, readOnly: false},
+                    magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                    restore : {show: true},
+                    saveAsImage : {show: true}
+                }
+            },
+
+            calculable : true,
+            xAxis : [
+                {
+                    type : 'category',
+
+                    axisTick:{show:false},
+
+                    boundaryGap : false,
+                    axisLabel: {
+                        textStyle:{
+                            color: '#ccc',
+                            fontSize:'12'
+                        },
+                        lineStyle:{
+                            color:'#2c3459',
+                        },
+                        interval: {default: 0},
+                        rotate:50,
+                        formatter : function(params){
+                            var newParamsName = "";// 最终拼接成的字符串
+                            var paramsNameNumber = params.length;// 实际标签的个数
+                            var provideNumber = 4;// 每行能显示的字的个数
+                            var rowNumber = Math.ceil(paramsNameNumber / provideNumber);// 换行的话，需要显示几行，向上取整
+                            /**
+                             * 判断标签的个数是否大于规定的个数， 如果大于，则进行换行处理 如果不大于，即等于或小于，就返回原标签
+                             */
+                            // 条件等同于rowNumber>1
+                            if (paramsNameNumber > provideNumber) {
+                                /** 循环每一行,p表示行 */
+                                var tempStr = "";
+                                tempStr=params.substring(0,4);
+                                newParamsName = tempStr+"...";// 最终拼成的字符串
+                            } else {
+                                // 将旧标签的值赋给新标签
+                                newParamsName = params;
+                            }
+                            //将最终的字符串返回
+                            return newParamsName
+                        }
+
+                    },
+                    data: ['0时','1时','2时','3时','4时','5时','6时','7时','8时','9时','10时','11时','12时','13时','14时','15时','16时','17时'
+                        ,'18时','19时','20时','21时','22时','23时']
+                }
+            ],
+            yAxis : {
+
+                type : 'value',
+                axisLabel: {
+                    textStyle: {
+                        color: '#ccc',
+                        fontSize:'12',
+                    }
+                },
+                axisLine: {
+                    lineStyle:{
+                        color:'rgba(160,160,160,0.3)',
+                    }
+                },
+                splitLine: {
+                    lineStyle:{
+                        color:'rgba(160,160,160,0.3)',
+                    }
+                },
+
+            }
+            ,
+            series : [
+                {
+                    // name:'简易程序案件数',
+                    type:'line',
+                    areaStyle: {
+
+                        normal: {type: 'default',
+                            color: new echarts.graphic.LinearGradient(0, 0, 0, 0.8, [{
+                                offset: 0,
+                                color: '#25f3e6'
+                            }, {
+                                offset: 1,
+                                color: '#0089ff'
+                            }], false)
+                        }
+                    },
+                    smooth:true,
+                    itemStyle: {
+                        normal: {areaStyle: {type: 'default'}}
+                    },
+                    data:[710, 312, 321,754, 500, 830, 710, 521, 504, 660, 530, 410,710, 312, 321,754, 500, 830, 710, 521, 504, 660, 530, 410]
+                }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
 	function leida1(){
 	var myChart = echarts.init(document.getElementById('map'));
 	
@@ -20,8 +246,6 @@ $(function(){
     });
 	 
 	}
-
-
 	
 	
 })
@@ -104,7 +328,7 @@ function map(){
 		        map: 'china',
 		        aspectScale: 0.85,
 		        layoutCenter: ["50%", "50%"], //地图位置
-		        layoutSize: '100%',
+		        layoutSize: '75%',
 		        itemStyle: {
 		            normal: {
 		                shadowColor: '#276fce',
